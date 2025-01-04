01bb710989908e05166e60e33fbcfdc3a91ff100
Vayun <vayunekbote02@gmail.com>
2025-01-04 18:21:45 +0530

    Completed first implementation of NotePage UI

--- src/components/NotePage.jsx
+++ src/components/NotePage.jsx
@@ -1,20 +1,74 @@
-import { CircleArrowLeft } from "lucide-react";
-import { useSelector } from "react-redux";
-import { NavLink, useParams } from "react-router";
+import {
+  CircleArrowLeft,
+  CircleCheck,
+  CirclePlus,
+  CircleX,
+  Edit,
+} from "lucide-react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useNavigate, useParams } from "react-router";
+import { ConfirmationModal } from "./ConfirmationModal";
+import { deleteNote } from "../features/notesSlice";
+import { clipTitle } from "../common_functions";
 
 export const NotePage = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const note = useSelector((state) =>
     state.notes.find((notes) => notes.id === id)
   );
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleDeleteNote = () => {
+    dispatch(deleteNote(id));
+    setModalOpen(false);
+    navigate("/");
+  };
 
   return (
     <>
       {note ? (
-        <div className="flex flex-col h-screen pt-8 pb-6 pl-8 pr-8 bg-white">
-          <h1 className="mb-4 text-3xl font-bold text-gray-800">
-            {note.title}
-          </h1>
+        <div className="flex flex-col h-screen px-8 pt-4 pb-6 bg-white">
+          <div className="flex items-center justify-between">
+            {/* Back button and title */}
+            <div className="flex items-center gap-5">
+              <NavLink
+                to="/"
+                className="pb-4 cursor-pointer"
+                title="Back to the homepage"
+              >
+                <CircleArrowLeft />
+              </NavLink>
+              <h1 className="mb-4 text-3xl font-bold text-gray-800">
+                {note.title}
+              </h1>
+            </div>
+            {/* Edit, Delete, Complete*/}
+            <div className="flex gap-4">
+              <button
+                className="text-green-600 hover:text-green-700"
+                title="Mark as completed"
+              >
+                <CircleCheck size={32} />
+              </button>
+
+              <button
+                className="text-green-600 hover:text-green-700"
+                title="Edit Note"
+              >
+                <Edit />
+              </button>
+              <button
+                className="text-red-600 btn hover:text-red-700"
+                title="Delete All Notes"
+                onClick={() => setModalOpen(true)}
+              >
+                <CircleX />
+              </button>
+            </div>
+          </div>
           <div className="flex-1 p-4 border-4 border-black border-dashed shadow-inner">
             {note.content}
           </div>
@@ -24,6 +78,14 @@ export const NotePage = () => {
         </div>
       ) : (
         <NoteNotFound />
+      )}
+      {modalOpen && (
+        <ConfirmationModal
+          isOpen={modalOpen}
+          onClose={() => setModalOpen(false)}
+          onConfirm={handleDeleteNote}
+          message={clipTitle(note.title, "modal")}
+        />
       )}
     </>
   );
